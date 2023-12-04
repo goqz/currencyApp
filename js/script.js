b9636fd2bdbb548705c1807b941a1dda0c83e39f
goqz <z.wyrozumski@gmail.com>
2023-12-04 11:32:09 +0100

    updated CSS, RWD, all working fine. Basic 1.0.0 version ready

--- js/script.js
+++ js/script.js
@@ -16,9 +16,22 @@ const currTwoValue = document.querySelector('#currTwoValue')
 const convert = document.querySelector('#convert')
 const currencyList = document.querySelector('#currencyList')
 const currencyListTwo = document.querySelector('#currencyListTwo')
-const currencyListDiv = document.querySelector('#currList')
-const currencyListTwoDiv = document.querySelector('#currListTwo')
+const currencyListContainer = document.querySelector('#currList')
+const currencyListTwoContainer = document.querySelector('#currListTwo')
 const errorInfo = document.querySelector('#errorInfo')
+
+const table = document.querySelector('#tableCurrency')
+const tableValue = document.querySelector('#tableValue')
+const tableCurr = document.querySelector('#tableCurr')
+const currListTableCont = document.querySelector('#currListTableContainer')
+const currencyListTable = document.querySelector('#currencyListTable')
+const tableCurrHidden = document.querySelector('#tableCurrHidden')
+const tablePush = document.querySelector('#tablePush')
+
+const tabOne = document.querySelector('#tabLeft')
+const tabTwo = document.querySelector('#tabRight')
+const mainBox = document.querySelector('#converterBox')
+const tableBox = document.querySelector('#tableBox')
 
 const currencies = [
     {
@@ -99,6 +112,7 @@ const createCurrencyList = (listElement) => {
 
 createCurrencyList(currencyList)
 createCurrencyList(currencyListTwo)
+createCurrencyList(currencyListTable)
 
 function isCurrencySymbol(zmienna) {
     const regex = /^[A-Z]{3}$/;
@@ -167,23 +181,66 @@ const handleKeyPress = (e, listDiv, inputElement, hiddenInput) => {
     }
 }
 
-const getRates = () => {
+const getRates = (cOne, cTwo, valueOne) => {
     // Inputs are hidden, but check just in case
-    if(!isCurrencySymbol(currTwoHidden.value)) return
-    if(!isCurrencySymbol(currOneHidden.value)) return
+    if(!isCurrencySymbol(cTwo)) return
+    if(!isCurrencySymbol(cOne)) return
 
-    const URL = API_LINK + API_KEY + API_UNITS + currTwoHidden.value + API_UNITS_2 + currOneHidden.value
+    const URL = API_LINK + API_KEY + API_UNITS + cTwo + API_UNITS_2 + cOne
 
     axios
         .get(URL)
         .then(res => {
-            if(Number.isNaN(currOneValue.value)) {
+            if(Number.isNaN(valueOne)) {
                 errorInfo.textContent = 'Musisz podać poprawną kwotę'
                 return
             } 
-            let exchange = parseFloat(res.data.data[currTwoHidden.value])
-            currTwoValue.value = Math.round(exchange * currOneValue.value * 100)/100
+            let exchange = parseFloat(res.data.data[cTwo])
+            currTwoValue.value = Math.round(exchange * valueOne * 100)/100
             errorInfo.textContent = ''
+        })
+        .catch(error => {
+            console.error('Błąd API:', error)
+            errorInfo.textContent = 'Wystąpił błąd.'
+        })
+}
+
+const getTable = (cOne, vOne) => {
+    const URL = API_LINK + API_KEY + API_UNITS + API_UNITS_2 + cOne
+    table.innerHTML = '<tr><th class="currency-symbol">Symbol</th><th class="currency-name">Waluta</th><th class="currency-value">Wartość</th></tr>'
+    axios
+        .get(URL)
+        .then(res => {
+            if(Number.isNaN(vOne)) {
+                errorInfo.textContent = 'Musisz podać poprawną kwotę'
+                return
+            } 
+            let i = 0
+            Object.entries(res.data.data).forEach(item => {
+                if(cOne === item[0]) {
+                } else {
+                    let exchange = parseFloat(item[1])
+                    currencies.forEach(newItem => {
+                        if(newItem['symbol'] === item[0]) {
+                            let newRow = document.createElement('tr')
+                            let firstColumn = document.createElement('td')
+                            let secondColumn = document.createElement('td')
+                            let thirdColumn = document.createElement('td')
+                            i % 2 == 0 ? newRow.classList.add('second-row') :
+                            firstColumn.classList.add('currency-symbol')
+                            secondColumn.classList.add('currency-name')
+                            thirdColumn.classList.add('currency-value')
+                            firstColumn.textContent = newItem['symbol']
+                            secondColumn.textContent = newItem['name']
+                            thirdColumn.textContent = Math.round(exchange * vOne * 100)/100
+                            newRow.append(firstColumn, secondColumn, thirdColumn)
+                            table.appendChild(newRow)
+                            i++
+                        }
+                    })
+                    
+                }
+            });
         })
         .catch(error => {
             console.error('Błąd API:', error)
@@ -198,15 +255,56 @@ const enterCheck = (e) => {
     }
 }
 
-convert.addEventListener('click', getRates)
-currOne.addEventListener('focus', () => showCurrencyList(currencyListDiv, currOne))
-currTwo.addEventListener('focus', () => showCurrencyList(currencyListTwoDiv, currTwo))
-currOne.addEventListener('blur', () => hideCurrencyList(currencyListDiv))
-currTwo.addEventListener('blur', () => hideCurrencyList(currencyListTwoDiv))
+convert.addEventListener('click', () => getRates(currOneHidden.value, currTwoHidden.value, currOneValue.value))
+
+currOne.addEventListener('focus', () => showCurrencyList(currencyListContainer, currOne))
+currTwo.addEventListener('focus', () => showCurrencyList(currencyListTwoContainer, currTwo))
+currOne.addEventListener('blur', () => hideCurrencyList(currencyListContainer))
+currTwo.addEventListener('blur', () => hideCurrencyList(currencyListTwoContainer))
 currOne.addEventListener('input', (e) => searchInList(e, currencyList))
 currTwo.addEventListener('input', (e) => searchInList(e, currencyListTwo))
 currOne.addEventListener('keyup', (e) => handleKeyPress(e, currencyList, currOne, currOneHidden))
 currTwo.addEventListener('keyup', (e) => handleKeyPress(e, currencyListTwo, currTwo, currTwoHidden))
-currencyList.addEventListener('click', (e) => chooseCurrency(e, currencyListDiv, currOne, currOneHidden))
-currencyListTwo.addEventListener('click', (e) => chooseCurrency(e, currencyListTwoDiv, currTwo, currTwoHidden))
+
+tableCurr.addEventListener('focus', () => showCurrencyList(currListTableCont, tableCurr))
+tableCurr.addEventListener('blur', () => hideCurrencyList(currListTableCont))
+tableCurr.addEventListener('input', (e) => searchInList(e, currencyListTable))
+tableCurr.addEventListener('keyup', (e) => handleKeyPress(e, currencyListTable, tableCurr, tableCurrHidden))
+
+currencyList.addEventListener('click', (e) => chooseCurrency(e, currencyListContainer, currOne, currOneHidden))
+currencyListTwo.addEventListener('click', (e) => chooseCurrency(e, currencyListTwoContainer, currTwo, currTwoHidden))
+currencyListTable.addEventListener('click', (e) => chooseCurrency(e, currListTableCont, tableCurr, tableCurrHidden))
+
+tablePush.addEventListener('click', () => getTable(tableCurrHidden.value, tableValue.value))
+
 currOneValue.addEventListener('keyup', enterCheck)
+tableValue.addEventListener('keyup', enterCheck)
+
+tabOne.addEventListener('click', () => {
+    if(tabOne.classList.contains('active-tab')) {
+
+    } else {
+        tabTwo.classList.remove('active-tab')
+        tabOne.classList.add('active-tab')
+        mainBox.classList.remove('hidden')
+        mainBox.classList.add('block-active')
+        tableBox.classList.add('hidden')
+        tableBox.classList.remove('block-active')
+        table.classList.add('hidden')
+        table.classList.remove('table-active')
+    }
+})
+tabTwo.addEventListener('click', () => {
+    if(tabTwo.classList.contains('active-tab')) {
+
+    } else {
+        tabTwo.classList.add('active-tab')
+        tabOne.classList.remove('active-tab')
+        mainBox.classList.add('hidden')
+        mainBox.classList.remove('block-active')
+        tableBox.classList.remove('hidden')
+        tableBox.classList.add('block-active')
+        table.classList.remove('hidden')
+        table.classList.add('table-active')
+    }
+})
